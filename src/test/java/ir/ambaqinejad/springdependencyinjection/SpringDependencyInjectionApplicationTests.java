2334ea658de1afab59e4a75d863fa1654fd10f42
ambaqinejad <ambaqinejad@gamil.com>
2024-11-27 21:06:02 +0330

    using dependency injection in test class

--- src/test/java/ir/ambaqinejad/springdependencyinjection/SpringDependencyInjectionApplicationTests.java
+++ src/test/java/ir/ambaqinejad/springdependencyinjection/SpringDependencyInjectionApplicationTests.java
@@ -1,10 +1,29 @@
 package ir.ambaqinejad.springdependencyinjection;
 
+import ir.ambaqinejad.springdependencyinjection.controller.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringDependencyInjectionApplicationTests {
+
+    @Autowired
+    ApplicationContext ctx;
+
+    @Autowired MyController controller;
+
+    @Test
+    void testAutowiredController() {
+        System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController controller = ctx.getBean(MyController.class);
+        System.out.println(controller.sayHello());
+    }
 
     @Test
     void contextLoads() {
