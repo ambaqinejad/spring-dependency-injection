c34dbf63dfbc750c780088d412fbaca78b5479ce
ambaqinejad <ambaqinejad@gamil.com>
2024-11-29 20:35:19 +0330

    Primary Beans

---
+++ src/main/java/ir/ambaqinejad/springdependencyinjection/services/GreetingServicePrimary.java
@@ -0,0 +1,13 @@
+package ir.ambaqinejad.springdependencyinjection.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class GreetingServicePrimary implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello from Primary Bean";
+    }
+}
